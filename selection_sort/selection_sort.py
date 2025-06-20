dec01387d7678e67748774dd9c301ef7cff58854
stil-e <brian.ikubu@strathmore.edu>
2025-06-20 10:38:48 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
+    def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
     pass
